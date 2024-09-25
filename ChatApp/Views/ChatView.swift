95914a956b55cd83738c998bc7f933aa73f79288
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-09-25 14:29:56 -0500

    creaacion del diseño de la vista usernameView, se creoaron los archivos chatView, messageView y sendView para completar el modelo de archivos que se utilizaran

---
+++ ChatApp/Views/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  ChatApp
+//
+//  Created by German David Vertel Narvaez on 25/09/24.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
---
+++ ChatApp/Views/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  ChatApp
+//
+//  Created by German David Vertel Narvaez on 25/09/24.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
