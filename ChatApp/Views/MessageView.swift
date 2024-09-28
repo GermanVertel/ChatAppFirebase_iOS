38e0bdc7384e21f87f56c92e957e777f8723c163
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-09-27 23:06:56 -0500

    diseño de la vista de los mensajes en la messageView

--- ChatApp/Views/MessageView.swift
+++ ChatApp/Views/MessageView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct MessageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: bubble ? .trailing : .leading ){
+            HStack{
+                Text(messages.text)
+                    .foregroundStyle(bubble ? Color.white : Color.black)
+                    .padding()
+                    .background()
+                    .background(bubble ? Color.blue : Color.gray)
+                    .cornerRadius(30)
+            }
+            .frame(maxWidth: 300, alignment: bubble ? .trailing : .leading)
+            
+            Text(messages.username)
+                .font(.caption)
+                .foregroundStyle(.gray)
+                .padding(bubble ? .trailing : .leading )
+                
+        }
+        .frame(maxWidth: .infinity, alignment: bubble ? .trailing : .leading)
+        .padding(bubble ? .trailing : .leading)
+        .padding(.horizontal, 10)
+        .onAppear {
+            let idUser = UserDefaults.standard.string(forKey: "idUser") // Asigna el valor de UserDefaults
+            
+            if idUser == messages.idUser { // Compara idUser con messages.idUser
+                bubble = true // Cambia el valor de bubble si coincide
+            }
+        }
+     
     }
 }
 
